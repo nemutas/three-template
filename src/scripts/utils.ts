e68ddb1d529a741ffebffa10e6af7cae3a8eb93f
nemutas <inohiro.focru@gmail.com>
2022-12-18 11:22:57 +0900

    init

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -1,12 +1,4 @@
-import { Page } from '../types/types'
-
 export const resolvePath = (path: string) => {
-	const p = path.startsWith('/') ? path.substring(1) : path
-	return import.meta.env.BASE_URL + p
-}
-
-export const registPageInstance = (page: Page) => {
-	window.addEventListener('beforeunload', () => {
-		page.dispose()
-	})
+  const p = path.startsWith('/') ? path.substring(1) : path
+  return import.meta.env.BASE_URL + p
 }
